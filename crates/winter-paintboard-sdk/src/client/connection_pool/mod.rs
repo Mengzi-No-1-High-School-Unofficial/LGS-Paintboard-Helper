9ef1cb8807f38b9e2ef3d4a1a02c7afa647e66bb
Xyber Nova <xyber-nova@outlook.com>
2025-10-19 20:49:40 +0800

    chore: 彻底移除 Connection Pool

--- crates/winter-paintboard-sdk/src/client/connection_pool/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod manager;
-pub mod monitoring;
-pub mod pool;
-pub mod pool_client;
-
-pub use manager::start_connection_manager_task;
-pub use monitoring::start_monitoring_task;
-pub use pool::{ConnectionGuard, ConnectionPool};
-pub use pool_client::{PoolClient, PoolMetrics};
